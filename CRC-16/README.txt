f6364b5d4a69ce166ffd7fd4d105b289e30519b3
Travis <travis.bashor@gmail.com>
2014-11-07 21:55:32 -0500

    complete crc-16 check/validation program

---
+++ CRC-16/README.txt
@@ -0,0 +1,8 @@
+Compilation Command: gcc -lm -o Checker crc.c
+Run Command: Checker c input.txt <--- for calculation mode
+             Checker v input.txt <--- for verification mode
+
+This program is entirely my own work and I have neither developed my code together with any other person,
+nor copied program code from any other person, nor permitted my code to be copied or otherwise used by any
+other person, nor have I copied, modified, or otherwise used program code that I have found in any external
+source, including but not limited to, online sources.
--- CRC-16/crc.c
+++ CRC-16/crc.c
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 
 // prototypes
+void validateInput(int, char*, char*);
+char* removeOffset(char*);
+void displayObservedCRC(char*);
+void displayCalculations(char*);
+void displayValues(char*, char*);
+int findNewIndex(char*);
 char* hexCharToBin(char);
 char* padBinaryString(char*);
 void printBinaryString(char*);
@@ -13,37 +19,246 @@ char fourBitToHex(char*);
 char* hexToBin(char*);
 bool validate(char*);
 char* xor(char*, char*);
-char* calcCRC(char*, char*);
+char* calcCRC(char*, char*, int);
 bool verifyCRC(char*, char*);
 char* assignHexFromFile(char*);
+char* appendZeroes(char*);
+char* gatherOffsetFromFile(char*);
+void printArgumentError();
+void printFileError();
+void printFlagError();
+void printValidationError();
+void runCalculation(char*, char*);
+void runVerification(char*, char*);
 
 int main(int argc, char** argv) {
   char* polynomial = "10100101010001101";
-  char* oneBinary = argv[1];
-  char* otherBinary = argv[2];
   char* flagValue;
   char* inputFile;
-  char* someHex = (char *)calloc(41, sizeof(char));
+
+  validateInput(argc, argv[1], argv[2]);
   
-  printf("The result of the XOR between the two values is: \n");
-  printBinaryString(xor(oneBinary, otherBinary));
-  // printf("The input file (bin):\n");
-  // printBinaryString(hexToBin(someHex));
-  // printf("\n");
+  // assign flagValue and inputFile from keyboard entry
+  flagValue = argv[1];
+  inputFile = argv[2];
 
-  // // state the polynomial used for the calculation
-  // printf("The polynomial that was used (binary bit string): 1010 0101 0100 0110 1\n");
-  // printf("We will append 16 zeroes at the end of the binary input.\n\n");
+  // run in calculation mode
+  if(strcmp(flagValue, "c") == 0) {
+    runCalculation(inputFile, polynomial);
+  }
+  // run in verification mode
+  else {
+    runVerification(inputFile, polynomial);
+  }
 
-  // // state the xors
-  // printf("The binary string difference after each XOR step of the CRC calculation:\n\n");
-  // printf("**Stuff goes here**\n\n");
-
-  // // compute the crc and display the results
-  // printf("The computed CRC for the input file is: SOMENUMBER\n");
   return 0;
 }
 
+// check that user input adheres to basic requirements
+void validateInput(int numArguments, char* firstArgument, char* secondArgument) {
+  // error-checking tree
+  if(numArguments != 3) {
+    printArgumentError();
+    exit(0);
+  }
+  else if(strcmp(firstArgument, "c") != 0 && strcmp(firstArgument, "v") != 0) {
+    printFlagError();
+    exit(0);
+  }
+  else if(!fopen(secondArgument, "r")) {
+    printFileError();
+    exit(0);
+  }
+  else if(!validate(secondArgument)) {
+    printValidationError();
+    exit(0);
+  }
+}
+
+// run the program in calculation mode
+void runCalculation(char* inputFile, char* polynomial) {
+  char* hexVal = (char *)calloc(41, sizeof(char));
+  char* computedCRC = (char *)calloc(5, sizeof(char));
+
+  // assign the hex value from the file
+  hexVal = assignHexFromFile(inputFile);
+
+  // declare the contents of the input file in hex and binary
+  displayValues(hexVal, polynomial);
+  printf("We will append 16 zeroes to the binary input.\n\n");
+
+  // calculate CRC-16
+  computedCRC = calcCRC(appendZeroes(hexToBin(hexVal)), polynomial, 1);
+
+  // display computed CRC
+  displayCalculations(computedCRC);
+}
+
+// run the program in verification mode
+void runVerification(char* inputFile, char* polynomial) {
+  char* hexVal = (char *)calloc(41, sizeof(char));
+  char* computedCRC = (char *)calloc(5, sizeof(char));
+  bool pass;
+
+  // assign the hex value from the file
+  hexVal = assignHexFromFile(inputFile);
+
+  // declare the contents of the input file in hex and binary
+  displayValues(hexVal, polynomial);
+
+  // declare the observed 16-bit crc
+  displayObservedCRC(inputFile);
+
+  // verify the crc
+  pass = verifyCRC(hexToBin(hexVal), polynomial);
+
+  // compute the CRC
+  computedCRC = calcCRC(appendZeroes(hexToBin(removeOffset(hexVal))), polynomial, 0);
+
+  // display result of calculations
+  displayCalculations(computedCRC);
+  printf("\n");
+
+  // display pass/fail
+  printf("Did the CRC check pass? (Yes or No): ");
+  if(pass) {
+    printf("Yes\n\n");
+  }
+  else {
+    printf("No\n\n");
+  }
+
+}
+
+// remove the observed CRC-16 from the hex value in the input file
+char* removeOffset(char* hexVal) {
+  char* newHex = (char *)calloc(37, sizeof(char));
+  int numDigits = strlen(hexVal);
+  int i;
+
+  for(i = 0; i < numDigits - 4; ++i) {
+    newHex[i] = hexVal[i];
+  }
+
+  return newHex;
+}
+
+// declare the CRC-16 attached to the value in the file
+void displayObservedCRC(char* inputFile) {
+  char* offset = (char *)calloc(5, sizeof(char));
+  offset = gatherOffsetFromFile(inputFile);
+
+  printf("The 16-bit CRC observed at the end of the file (bin):\n");
+  printBinaryString(hexToBin(offset));
+  printf("\n");
+  printf("The 16-bit CRC observed at the end of the file (hex):\n%s\n\n", offset);
+}
+
+// show the computed CRC in binary and hex
+void displayCalculations(char* computedCRC) {
+  printf("The computed CRC for the input file (bin):\n");
+  printBinaryString(computedCRC);
+  printf("\n");
+  printf("The computed CRC for the input file (hex):\n%s\n", binToHex(computedCRC));
+}
+
+// display the hex and binary versions of the values to the consol
+void displayValues(char* hexVal, char* polynomial) {
+  // declare the contents of the input file in hex and binary
+  printf("The input file (hex):\n%s\n\n", hexVal);
+  printf("The input file (bin):\n");
+  printBinaryString(hexToBin(hexVal));
+  printf("\n\n");
+
+  // declare the polynomial used for calculation
+  printf("The polynomial that was used (binary bit string):\n");
+  printBinaryString(polynomial);
+}
+
+// error message for non-hexadecimal digits found in file
+void printValidationError() {
+  printf("The file specified contains non-hexadecimal digits.\n");
+}
+
+// print error message for wrong number of arguments
+void printArgumentError() {
+  printf("You must enter 2 arguments:\n");
+  printf("1) 'c' or 'v' for calculate or verify a CRC-16, respectively\n");
+  printf("2) the name of a file (such as input.txt) to be used");
+}
+
+// print error message for input file
+void printFileError() {
+  printf("The system cannot find the file specified.\n");
+}
+  
+// print error message for wrong flag entry
+void printFlagError() {
+  printf("You must choose either a 'c' or a 'v' for your first argument.\n");
+}
+
+// verify the validity of the crc appended to the message
+bool verifyCRC(char* binaryString, char* polynomial) {
+  char* properResult = "0000000000000000";
+
+  if(strcmp(calcCRC(binaryString, polynomial, 1), properResult) == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// calculates the crc-16 value for the given binaryString, polynomial pair
+char* calcCRC(char* binaryString, char* polynomial, int displayResults) {
+  if(displayResults == 1) {
+    printf("The binary string difference after each XOR step of the CRC calculation:\n\n");
+    printBinaryString(binaryString);
+  }
+  int i = 0;
+  char* result = (char *)calloc(strlen(binaryString) + 1, sizeof(char));
+
+  // initialize result as the binary string before any operations
+  result = binaryString;
+  while(i < strlen(binaryString) - 17) {
+    i = findNewIndex(result);
+    strncpy(result + i, xor(polynomial, result + i), strlen(polynomial));
+    if(displayResults == 1) {
+      printBinaryString(result);
+    }
+  }
+
+  if(displayResults == 1) {
+    printf("\n");
+  }
+  return (result + (strlen(binaryString) - 16));
+}
+
+// finds the next non-zero digit during polynomial division
+int findNewIndex(char* binaryString) {
+  int index;
+
+  for(index = 0; index < strlen(binaryString) - 17; ++index) {
+    if(binaryString[index] != '0') {
+      break;
+    }
+  }
+
+  return index;
+}
+
+// append 16 zeroes to our binary string
+char* appendZeroes(char* binaryString) {
+  char* newBinary = (char *)calloc(strlen(binaryString) + 17, sizeof(char));
+  char* sixteenZeroes = "0000000000000000";
+
+  strcpy(newBinary, binaryString);
+  strcat(newBinary, sixteenZeroes);
+
+  return newBinary;
+}
+
+// shows the result of XOR on two binary strings
 char* xor(char* binA, char* binB) {
   int i;
   char* binC = (char *)calloc(strlen(binA) + 1, sizeof(char));
@@ -61,6 +276,7 @@ char* xor(char* binA, char* binB) {
   return binC;
 }
 
+// auxillary function for verification mode
 bool validate(char* inputFile) {
   int i;
   char* fileContents = (char *)calloc(41, sizeof(char));
@@ -85,6 +301,24 @@ bool validate(char* inputFile) {
   }
 
   return goodFile;
+}
+
+// pulls the offset from the file when in verification mode
+char* gatherOffsetFromFile(char* inputFile) {
+  char* fileContents = (char *)calloc(41, sizeof(char));
+  char* offset = (char *)calloc(5, sizeof(char));
+  int fileLength;
+  FILE* ifp = fopen(inputFile, "r");
+
+  // gather contents
+  fscanf(ifp, "%s", fileContents);
+  fileLength = strlen(fileContents);
+
+  // pull last four digits from file
+  strncpy(offset, (fileContents + fileLength - 4), 4);
+
+  fclose(ifp);
+  return offset;
 }
 
 // return the hexadecimal value in the file
